88c1fae9fef7f0367b10157c65ad396117fab68d
SanttuHaka <aleksanteri.haka@gmail.com>
2023-11-28 13:43:07 +0200

    Redirect fix

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -64,6 +64,13 @@ export const authOptions = {
       // you might return this in new version
       return session;
     },
+    async redirect({ url, baseUrl }) {
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
+    },
   },
 };
 
