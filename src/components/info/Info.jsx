8ac8cec99b8f7c22f617e05b8d16e15f6a0e4bae
khelmeri <154372614+khelmeri@users.noreply.github.com>
2025-12-07 15:52:00 +0200

    rivi 47 objectFit: "none".jsx

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -44,7 +44,7 @@ export default function ({ infoData }) {
         sizes=""
         fill
         style={{
-          objectFit: "cover",
+          objectFit: "none",
         }}
         className="z-0 brightness-50 grayscale"
         alt="hero"
