216b7710e79132e0ff8c2382d1f4ebffa560aca3
khelmeri <154372614+khelmeri@users.noreply.github.com>
2025-12-07 16:18:21 +0200

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,7 +15,7 @@ export default function ({ contactData }) {
         style={{
           objectFit: "cover",
         }}
-        className="z-0 brightness0 greyscale"
+        className="z-0 brightness0 grayscale"
         alt="hero"
       />
 
